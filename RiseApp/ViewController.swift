825fb93f90a86c2006f8a278e2dacdb200bd095e
Deniz <denizm@valensas.com>
2018-06-21 00:18:15 +0300

    Initial Commit

---
+++ RiseApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RiseApp
+//
+//  Created by Deniz Mersinlioğlu on 21.06.2018.
+//  Copyright © 2018 ArcheTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
