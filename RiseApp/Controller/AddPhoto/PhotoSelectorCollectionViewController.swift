f9746fc187c7a616bd9bdbbdb0f85e02fab2a131
Deniz <denizm@valensas.com>
2018-07-09 22:39:21 +0300

    photo selection done

--- RiseApp/Controller/AddPhoto/PhotoSelectorCollectionViewController.swift
+++ RiseApp/Controller/AddPhoto/PhotoSelectorCollectionViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class PhotoSelectorCollectionViewController: UICollectionViewController {
+class PhotoSelectorCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    fileprivate let cellID = "cellID"
+    fileprivate let headerID = "headerID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = .yellow
         
         setupNavigationButtons()
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerID)
     }
+    
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -33,18 +39,46 @@ class PhotoSelectorCollectionViewController: UICollectionViewController {
     }
     
     @objc func handleNext(){
-        
+        print("Handling next")
     }
 }
 
 extension PhotoSelectorCollectionViewController{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 1, left: 0, bottom: 0, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        let width = view.frame.width
+        return CGSize(width: width, height: width)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerID, for: indexPath)
+        header.backgroundColor = .red
+        return header
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (view.frame.width - 3) / 4
+        return CGSize(width: width, height: width)
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .blue
         return cell
     }
 }
